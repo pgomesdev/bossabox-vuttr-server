a3a6acc6b42ed16114fc6a9282316934067b7dcb
pgomesdev <pgomesdev@gmail.com>
2020-05-09 11:41:30 -0300

    fix(app): removed tools import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ToolsModule } from './tools/tools.module';
 
 @Module({
-  imports: [ToolsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
